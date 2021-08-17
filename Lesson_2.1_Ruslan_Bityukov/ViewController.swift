7422970bdb10aab0cd8ff9773214223f8194f8dd
Ruslan Bityukov <bitukov_ruslan@mail.ru>
2021-08-17 18:41:18 +0300

    Cours 2.1 lesson 1.2

---
+++ Lesson_2.1_Ruslan_Bityukov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_2.1_Ruslan_Bityukov
+//
+//  Created by Руслан Битюков on 17.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
